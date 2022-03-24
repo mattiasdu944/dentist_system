35db9abea2315e84b08d208dea80fdfc4c605017
Ultron <duartemattias4@gmail.com>
2022-03-24 19:53:13 -0400

    Se agrego el navbar responsivo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,26 @@
+const navMenu = document.getElementById('nav-menu');
+const btnMenu = document.getElementById('btn-menu');
 const navbar = document.getElementById('navbar');
+const navToogle = document.getElementById('navId');
 
+/*===== MENU SHOW =====*/ 
+navToogle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-nav');
+});
+
+btnMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show-nav');
+});
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*========== NAVBAR SCROLL ==========*/
 window.addEventListener('scroll', () => {
